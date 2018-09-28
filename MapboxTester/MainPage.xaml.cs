291dabec6d2f98d34690b466e3cb0052ba4dc81c
Kimson Wong <kimsonwong@gmail.com>
2018-09-28 14:50:05 +0800

    - Normal pins position changed

--- MapboxTester/MainPage.xaml.cs
+++ MapboxTester/MainPage.xaml.cs
@@ -19,7 +19,7 @@ namespace MapboxTester
 
 			map.pins.Add(new Pin {
 				image = "Resources.car.png",
-				IsCenterAndFlat = true,
+				IsCenterAndFlat = false,
 				heading = 0,
 				position = new Position(0.5, -1)
 			});
@@ -42,17 +42,22 @@ namespace MapboxTester
 				position = new Position(-1, -1)
 			});
 
-			Device.StartTimer(TimeSpan.FromSeconds(8), () => {
-				map.moveMapToRegion(CameraPerspectiveFactory.fromCoordinatesAndPadding(
-					new List<Position>{
-						map.pins[0].position,
-						map.pins[1].position,
-						map.pins[2].position,
-						map.pins[3].position
-					},
-					new Thickness(50+25, 50+25, 50+25, 50+25)));
+			Device.StartTimer(TimeSpan.FromSeconds(10), () => {
+				map.pins[0].position = new Position(10, 123);
 				return false;
 			});
+
+			//Device.StartTimer(TimeSpan.FromSeconds(8), () => {
+			//	map.moveMapToRegion(CameraPerspectiveFactory.fromCoordinatesAndPadding(
+			//		new List<Position>{
+			//			map.pins[0].position,
+			//			map.pins[1].position,
+			//			map.pins[2].position,
+			//			map.pins[3].position
+			//		},
+			//		new Thickness(50+25, 50+25, 50+25, 50+25)));
+			//	return false;
+			//});
 			////map.initialCameraUpdate = CameraPerspectiveFactory.fromCenterAndZoomLevel(new Position(10.317119, 123.764238), 10);
 			////map.moveMapToRegion(CameraPerspectiveFactory.fromCenterAndZoomLevel(new Position(10.317119, 123.764238), 10));
 			//// Initialize pins
